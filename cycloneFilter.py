e09c1aeeeb438b594faea8e4e162ab62b27b63e1
Joshua Ward <jward@illuminate.solutions>
2019-10-25 12:35:07 -0700

    initial commit

---
+++ cycloneFilter.py
@@ -0,0 +1,138 @@
+from solid import *
+from solid.utils import *
+from math import sin, cos, radians, degrees
+from intakeManifold import extrude_intake_manifold
+
+# TODO: set segments on all cylindric primitives for smooth models
+
+# TODO: ensure the intakeRadius holes are consistent wrt wallWidth
+# TODO: reducing r^2 - wallWidth^2  =? (l-wallwidth)*(w-wallWidth)
+# (l-wallwidth)*(w-wallWidth) =  lw - lWall - wWall + wallWidth^2
+#  =lw - wall(l+w) + wallWidth^2
+# therefore: wallWidth of rectangle constrains linearly faster than wallWidth of radius
+# Corr: reduce wallWidth of rectangle by wallWidth but add back wall(l+w)
+# OR: consider wallWidth added to hole defined by intake
+
+# TODO: verify wallWidth addition to solids works
+
+
+# DESIGN GOAL:
+# create an object that will possibly be used for:
+# 1. design optimization for wetScrubber and/or vortex filterArray
+# 2. grid array iteration (for instance: iterating for effective paricle diameter in
+#  sequential-parallel array given a volume/manifold geometry)
+
+
+def ConeFilter(
+        intakeSlitLength,
+        intakeSlitWidth,
+        intakeSlitSize,
+        intakeLeft,
+        vortexSearcherDepth,
+        collectorDepth,
+        cylinderRadius,
+        cylinderHeight,
+        wallWidth
+):
+    # TODO: pir^2 must be greater than intake cross section where r is cylinderRadius.
+    # need more safety constraints.
+    '''
+    Creates a Cone filter hull
+    Parameters:
+        intakeSlitLength: air intake nozzle height
+        intakeSlitWidth: air intake nozzle width, should be minimized to particle/blob diameter
+        intakeSlitSize: length of intake nozzle tube connecting and transforming the intake tube to a slit
+        intakeLeft: if True, put intake on left side of filter, else place on right. 
+        useful for symmetric arrays in rectangular manifolds.
+        vortexSearcherDepth: sets depth of vortex (considered only past intakeSlitLength)
+        collectorDepth: depth of collector cone after cylinder
+        cylinderRadius: radius of cylinder that makes up the hull
+        cylinderHeight: height of cylinder that makes up the hull
+        wallWidth: width of wall for all parts
+    '''
+    ############# Build solids. #############
+    intakeRadius = sqrt((intakeSlitLength*intakeSlitWidth+wallWidth)/pi)
+    # build each part
+    mainBodySolid = cylinder(r=(cylinderRadius + wallWidth),
+                             h=(cylinderHeight+wallWidth))
+    collectorConeSolid = cylinder(
+        r1=(cylinderRadius + wallWidth), r2=(intakeRadius + wallWidth), h=collectorDepth)
+    vortexTubeSolid = cylinder(
+        r=(intakeRadius + wallWidth), h=(vortexSearcherDepth + intakeSlitLength + wallWidth), segments=100)
+    # currently using a slit of unit width.
+    # set width to expected particle/blob size
+    # TODO:  move these into parameters for filter
+    intakeSolid = extrude_intake_manifold(
+        # TODO: should this be parameterized or just as much as possible? \
+        # constants have no place in parametric models
+        intake_resolution=100,
+        exhaust_slit=intakeSlitLength + wallWidth,
+        exhaust_width=intakeSlitWidth + wallWidth,
+        exhaust_length=intakeSlitSize)  # TODO: consider extracting this
+
+    ############# Open holes inside solids#############
+    # becuase we parameterized by radius, wall width can be subtracted directly
+    mainBody = mainBodySolid - \
+        cylinder(r=cylinderRadius-wallWidth, h=cylinderHeight-wallWidth)
+    collectorCone = collectorConeSolid - \
+        hole()(cylinder(r1=cylinderRadius-wallWidth,
+                        r2=intakeRadius-wallWidth, h=collectorDepth))
+    vortexTube = vortexTubeSolid - \
+        hole()(cylinder(r=(intakeRadius - wallWidth),
+                        h=(vortexSearcherDepth + intakeSlitLength + wallWidth), segments=100))
+    intake = intakeSolid - hole()(extrude_intake_manifold(
+        intake_resolution=100,
+        exhaust_slit=intakeSlitLength-wallWidth,
+        exhaust_width=intakeSlitWidth-wallWidth,
+        exhaust_length=intakeSlitSize))
+
+    ############# Assemble filter: #############
+    # mainBody
+    if(intakeLeft is True):
+        filter = mainBody + \
+            rotate([180, 0, 0])(collectorCone) + \
+            up(cylinderHeight - (vortexSearcherDepth + intakeSlitLength))(vortexTube) + \
+            left(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitLength/2 - wallWidth)
+                                                   (rotate([90, 90, 0])(intake)))
+    else:
+        filter = mainBody + \
+            rotate([180, 0, 0])(collectorCone) + \
+            up(cylinderHeight - (vortexSearcherDepth + intakeSlitLength))(vortexTube) + \
+            right(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitLength/2 - wallWidth)
+                                                    (rotate([90, 90, 0])(intake)))
+    # TODO this is bad! need to parameterize width. only allowed because
+    # currently only supporting slit with 1 width!
+    # TODO: cylHeight-crossSection should be seekerOffset var
+
+    return filter
+
+
+############# Build Filter #############
+solution = ConeFilter(
+    intakeSlitLength=10, intakeSlitWidth=2, intakeSlitSize=20,
+    intakeLeft=True, vortexSearcherDepth=5, collectorDepth=100,
+    cylinderRadius=10, cylinderHeight=15, wallWidth=0.75)
+# Optimization Considerations:
+# minimize: cylinderHeight constrained by intakeSlitLength
+# minimize: vortexSearcherDepth constrained by cylinderHeight (only increased based on low pressure aerodynamics)
+# minimize: intakeSlitWidth: by particle diameter
+# minimize: cylinderRadius for volume, otherwise this would be maximized (design constraint)
+# minimize: wallWidth wrt pressure and material compressive strength
+# maximize: intakeSlitLength
+# maximize: collectorDepth ideally to infinity if pressure is infinity (perfect vacuum)
+# TODO: consider a higher level, safer parameterization for these considerations:
+#               remove: cylinderHeight, cylinderRadius by intakeArea, vortexSearcherDepth(after CFD analysis wrt pressure and intake model),
+#                             |_need to be constrained by pressure models
+
+# FUTURE DEVELOPMENT:
+#   set minimum and maximum values for parameters
+#  (so they dont create unsustainable models e.g.: intakeSlit>cylinderHeight)
+#  then run optimization algorithm on it wrt CFD (consider FreeCAD)
+#  also constrain the parameters to reduce testing (discretize and limit parameter space)
+
+############# Writeout Filter #############
+scad_render_to_file(
+    solution,
+    "cycloneFilter.scad",
+    "C:/Users/jw.local/Desktop/code/kinesisDataAnalytics//",
+)
---
+++ intakeManifold.py
@@ -0,0 +1,78 @@
+from solid import *
+from solid.utils import *
+from math import sin, cos, radians, degrees
+# Encapsulated seperate from cyclone hull due to complexity and reuse of code for vacuumManifold
+
+# TODO: put this back into camelCase because snakes are for dummies
+# TODO: should constraint safety be implemented here? in extrude_intake_manifold?
+# r = viewscad.Renderer(
+#     openscad_exec="C:\Program Files\OpenSCAD\openscad.exe"
+# )  # Point this to your openscad executable
+
+
+def construct_polygon(sides, radius_major, radius_minor, offset=0, ):
+    """
+    Builds a planar regular polygon of unit depth with a given offset
+
+    Parameters:
+        polyPoints: number of points for the reglar polygon
+        radius: the radius of a circle sweeping the polygon points
+        offset: shift along z axis, useful for extrusions
+    Returns:
+        a list of Point3 entries defining the regular polygon
+    """
+    geometry = []
+    for i in range(0, sides):
+        angle = i * 360 / sides
+        x = radius_major * sin(radians(angle))
+        y = radius_minor * cos(radians(angle))
+
+        geometry.append(Point3(x, y, offset))
+    return geometry
+
+
+def extrude_intake_manifold(intake_resolution, exhaust_slit, exhaust_width, exhaust_length):
+    '''
+    Extrudes an intake manifold for cyclone filtration.
+    Parameters:
+        intake_resolution: how many segments to be used for the final polygon (circle theroetically has infinite)
+        exhaust_slit: length of slit that enters the vortex
+        exhaust_width: width of slit that enters the vortex and also acts as cyclone guide (minimize)
+        exhaust_length: entire length of intake manifold
+    '''
+    inlet_radius = sqrt(exhaust_slit*exhaust_width/pi)
+
+    segmentedIntakeManifold = []  # return value
+    # constants are okay here as this module is meant to be transformed from origin
+    segmentedIntakeManifold.append(
+        down(1)(scale([exhaust_slit, exhaust_width, 1])(cube(center=True)))
+    )  # exhaust square (initial object)
+    raderator_major = (exhaust_slit - inlet_radius)/intake_resolution
+    raderator_minor = (exhaust_width - inlet_radius)/intake_resolution
+
+    inlet_radius_major = exhaust_slit/2
+    inlet_radius_minor = exhaust_width/2
+
+    length_iterator = exhaust_length/intake_resolution
+
+    for seg in range(1, intake_resolution):
+        inlet_radius_major -= raderator_major/2
+        inlet_radius_minor -= raderator_minor/2
+        length_iterator += exhaust_length/intake_resolution
+        segmentedIntakeManifold.append(
+            extrude_along_path(
+                construct_polygon(seg + 1,
+                                  inlet_radius_major, inlet_radius_minor, length_iterator),
+                [[0, 0, length_iterator], [0, 0, length_iterator + length_iterator]],
+            )
+        )
+    return hull()(segmentedIntakeManifold)
+
+############# Write to File (for logging) #############
+# solution = extrude_intake_manifold()
+# scad_render_to_file(
+#     solution,
+#     "segmentedManifold.scad",  # WAS: .scad
+#     "C:/Users/jw.local/Desktop/code/kinesisDataAnalytics/",
+# )
+################################################
