a085282d5f1999ecbf98312d9b6a69703ba94085
Josh Ward <ward.joshua92@yahoo.com>
2020-03-12 15:28:23 -0700

    add notebook and refactor some parameter names

--- cycloneFilter.py
+++ cycloneFilter.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
 from solid import *
 from solid.utils import *
 from math import sin, cos, radians, degrees
-from . import extrude_intake_manifold
 
-#############Static Constants#############
-CircleResolution = 100
+from intake import extrude_intake_manifold
+import viewscad
+
+
+
+#############Static Config#############
+CircleResolution = 100 
+
 
 # TODO: verify wallWidth addition to solids works
 #               (area is no less than intakeRadius throughout the model)
@@ -19,9 +30,9 @@ CircleResolution = 100
 
 
 def cycloneFilter(
-        intakeSlitLength,
+        intakeSlitHeight,
         intakeSlitWidth,
-        intakeSlitSize,
+        intakeSlitLength,
         intakeLeft,
         vortexSearcherDepth,
         collectorDepth,
@@ -34,9 +45,9 @@ def cycloneFilter(
     '''
     Creates a Cone filter hull
     Parameters:
-        intakeSlitLength: air intake nozzle height
+        intakeSlitHeight: air intake nozzle height
         intakeSlitWidth: air intake nozzle width, should be minimized to particle/blob diameter
-        intakeSlitSize: length of intake nozzle tube connecting and transforming the intake tube to a slit
+        intakeSlitLength: length of intake nozzle tube connecting and transforming the intake tube to a slit
         intakeLeft: if True, put intake on left side of filter, else place on right. 
         useful for symmetric arrays in rectangular manifolds.
         vortexSearcherDepth: sets depth of vortex (considered only past intakeSlitLength)
@@ -47,51 +58,43 @@ def cycloneFilter(
     '''
     ############# Build Solids: #############
     # WAS: +wallWidth
-    intakeRadius = sqrt((intakeSlitLength*intakeSlitWidth)/pi)
+    intakeRadius = sqrt((intakeSlitHeight*intakeSlitWidth)/pi)
+    
     # build each part
     mainBodySolid = cylinder(r=(cylinderRadius + wallWidth),
                              h=(cylinderHeight+wallWidth), segments=CircleResolution)
     collectorConeSolid = cylinder(
         r1=(cylinderRadius + wallWidth), r2=(intakeRadius + wallWidth), h=collectorDepth, segments=CircleResolution)
     vortexTubeSolid = cylinder(
-        r=(intakeRadius + wallWidth), h=(vortexSearcherDepth + intakeSlitLength), segments=CircleResolution)
+        r=(intakeRadius + wallWidth), h=(vortexSearcherDepth + intakeSlitHeight), segments=CircleResolution)
     intakeSolid = extrude_intake_manifold(
         # TODO: constants have no place in parametric models
         intake_resolution=100,
-        exhaust_slit=intakeSlitLength + wallWidth,
+        exhaust_slit=intakeSlitHeight + wallWidth,
         exhaust_width=intakeSlitWidth + wallWidth,
-        exhaust_length=intakeSlitSize)
+        exhaust_length=intakeSlitLength)
 
     ############# Open holes inside solids: #############
     # becuase we parameterized by radius, wall width can be subtracted directly
-    mainBody = mainBodySolid - \
-        cylinder(r=cylinderRadius, h=cylinderHeight, segments=CircleResolution)
-    collectorCone = collectorConeSolid - \
-        hole()(cylinder(r1=cylinderRadius,
+    mainBody = mainBodySolid -         cylinder(r=cylinderRadius, h=cylinderHeight, segments=CircleResolution)
+    collectorCone = collectorConeSolid -         hole()(cylinder(r1=cylinderRadius,
                         r2=intakeRadius, h=collectorDepth, segments=CircleResolution))
-    vortexTube = vortexTubeSolid - \
-        hole()(cylinder(r=(intakeRadius),
-                        h=(vortexSearcherDepth + intakeSlitLength), segments=CircleResolution))
+    vortexTube = vortexTubeSolid -         hole()(cylinder(r=(intakeRadius),
+                        h=(vortexSearcherDepth + intakeSlitHeight), segments=CircleResolution))
     intake = intakeSolid - hole()(extrude_intake_manifold(
         intake_resolution=100,
-        exhaust_slit=intakeSlitLength,
+        exhaust_slit=intakeSlitHeight,
         exhaust_width=intakeSlitWidth,
-        exhaust_length=intakeSlitSize))
+        exhaust_length=intakeSlitLength))
 
     ############# Assemble filter: #############
     # mainBody
     # TODO: remove rotates where appropriate
     if(intakeLeft is True):
-        filter = mainBody + \
-            rotate([180, 0, 0])(collectorCone) + \
-            up(cylinderHeight - (vortexSearcherDepth + intakeSlitLength-wallWidth))(vortexTube) + \
-            left(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitLength/2 - wallWidth)
+        filter = mainBody +             rotate([180, 0, 0])(collectorCone) +             up(cylinderHeight - (vortexSearcherDepth + intakeSlitHeight-wallWidth))(vortexTube) +             left(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitHeight/2 - wallWidth)
                                                    (rotate([90, 90, 0])(intake)))
     else:
-        filter = mainBody + \
-            rotate([180, 0, 0])(collectorCone) + \
-            up(cylinderHeight - (vortexSearcherDepth + intakeSlitLength-wallWidth))(vortexTube) + \
-            right(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitLength/2 - wallWidth)
+        filter = mainBody +             rotate([180, 0, 0])(collectorCone) +             up(cylinderHeight - (vortexSearcherDepth + intakeSlitHeight-wallWidth))(vortexTube) +             right(cylinderRadius - intakeSlitWidth)(up(cylinderHeight - intakeSlitHeight/2 - wallWidth)
                                                     (rotate([90, 90, 0])(intake)))
 
     return filter
@@ -99,7 +102,7 @@ def cycloneFilter(
 
 ############# Build Filter: #############
 solution = cycloneFilter(
-    intakeSlitLength=10, intakeSlitWidth=2, intakeSlitSize=20,
+    intakeSlitHeight=10, intakeSlitWidth=2, intakeSlitLength=20,
     intakeLeft=True, vortexSearcherDepth=5, collectorDepth=75,
     cylinderRadius=10, cylinderHeight=15, wallWidth=0.05)
 
@@ -122,12 +125,11 @@ solution = cycloneFilter(
 #  then run optimization algorithm on it wrt CFD (consider FreeCAD)
 #  also constrain the parameters to reduce testing (discretize and limit parameter space)
 
+
 ############# Writeout Filter Model #############
 scad_render_to_file(
     solution,
     "cycloneFilter.scad",
     # "PUT THE PATH TO YOUR OPENSCAD .EXE HERE",
 
-    # REMOVE THIS WHEN COMMITING
-    #    "C:/Users/jw.local/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/OpenSCAD.exe",
 )
